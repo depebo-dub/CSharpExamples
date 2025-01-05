a26cd761d127446d77f52786255fecfd9392ffaa
DESKTOP-190I4NK\Роман <depebo.dub@gmail.com>
2025-01-05 20:39:16 +0700

    add app

---
+++ DelegateConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DelegateConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void Hello(string a) => Console.WriteLine("Hello METANIT.COM" + a);
+
+            Hello("sdfg");
+
+
+            Console.WriteLine("1");
+        }
+    }
+}
